18f5bb9e5ba61aa7c28db2aabffe68651e06b22f
Ramasami <77200249+Ramasami@users.noreply.github.com>
2023-12-06 23:35:55 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (25.03%)

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode topNode = head;
+        ListNode prevNode = head;
+        ListNode currNode = topNode.next;
+        while(currNode != null) {
+            if (currNode.val != prevNode.val) {
+                prevNode.next = currNode;
+                prevNode = currNode;
+            }
+            currNode = currNode.next;
+        }
+        prevNode.next = null;
+        return topNode;
+    }
+}
