a208e0cd9607942404606351d933e3580178387e
Ramasami <77200249+Ramasami@users.noreply.github.com>
2023-12-06 21:22:28 +0530

    Time: 4 ms (100.00%), Space: 43.1 MB (34.21%)

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,12 +3,10 @@ class Solution {
         if (x<0 || (x !=0 &&x%10 == 0))
             return false;
         int y = 0;
-        while(x>=y) {
-            if (x == y || (x/10 == y))
-                return true;
+        while(x>y) {
             y = y*10+x%10;
             x/=10;
         }
-        return false;
+        return x == y || y/10 == x;
     }
 }
