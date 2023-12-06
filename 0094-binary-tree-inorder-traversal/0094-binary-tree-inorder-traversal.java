234be7c6502fe80a4f4c7f536ff759856b2faaca
Ramasami <77200249+Ramasami@users.noreply.github.com>
2023-12-06 23:36:40 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (35.51%)

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        return travel(root, list);
+    }
+    
+    private List<Integer> travel(TreeNode root, List<Integer> list) {
+        if (root == null)
+            return list;
+        list = travel(root.left, list);
+        list.add(root.val);
+        list = travel(root.right, list);
+        return list;
+    }
+}
