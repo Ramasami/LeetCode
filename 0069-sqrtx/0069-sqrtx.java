a16a5e2eae997786157c199760aa54c4932e979c
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-02 23:44:49 +0530

    Time: 1 ms (90.64%), Space: 41 MB (20.96%)

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,7 +1,9 @@
 class Solution {
     public int mySqrt(int x) {
-        long l = 0;
-        long r = x;
+        if(x<2)
+            return x;
+        long l = 1;
+        long r = x/2;
         while(l<r) {
             long m = l+(r-l)/2;
             if(x<m*m) r = m-1;
