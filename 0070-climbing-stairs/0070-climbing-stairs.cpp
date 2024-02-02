76be680c99f09271f9bb3b2e9ba27e6fd59f8c87
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-02 23:51:46 +0530

    Time: 0 ms (100.00%), Space: 7 MB (8.90%)

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int climbStairs(int n) {
         if (n ==1)
-            return n;
-        int twoBehind = 0;
-        int oneBehind = 1;
-        for(int i=2;i<=n;i++) {
+            return 1;
+        int twoBehind = 1;
+        int oneBehind = 2;
+        for(int i=3;i<=n;i++) {
             int curr = twoBehind + oneBehind;
             twoBehind = oneBehind;
             oneBehind = curr;
         }
-        return twoBehind + oneBehind;
+        return oneBehind;
     }
 };
