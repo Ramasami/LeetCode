128d542a49ffb895e1623dec8bcb420ce4acbd3b
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-03 22:49:41 +0530

    Time: 3 ms (43.34%), Space: 6.9 MB (27.93%)

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     bool isHappy(int n) {
         while(n!=1 && n!=4) {
-            int sum = 0;
             int t = n;
+            n = 0;
             while(t!=0) {
                 int digit = t%10;
                 t/=10;
-                sum+=digit*digit;
+                n+=digit*digit;
             }
-            n = sum;
         }
         return n == 1;
     }
