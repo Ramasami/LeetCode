290b3a6e131d0113680feafadbc6f3d61eaf2d39
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-03 15:18:25 +0530

    Time: 5 ms (19.24%), Space: 7.7 MB (26.24%)

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -3,7 +3,7 @@ public:
     int titleToNumber(string s) {
         long ans = 0;
         for(int i=0;i<s.length();i++)
-            ans = ((ans * 26) + s[i] - 64);
+            ans = ((ans * 26) - 64 + s[i]);
         return ans;
     }
 };
