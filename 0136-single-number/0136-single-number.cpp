5b35c39449a4721949d92c5cdb356fc7ff6aa6d0
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-03 12:55:56 +0530

    Time: 11 ms (87.97%), Space: 19.2 MB (31.54%)

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int x = 0;
-        for(vector<int>::iterator it = nums.begin();it!=nums.end();it++) {
-            x^=*it;
+        for(int i = 0;i<nums.size();i++) {
+            x^=nums[i];
         }
         return x;
     }
