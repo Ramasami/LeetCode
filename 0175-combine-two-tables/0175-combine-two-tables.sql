10ee11e146e554f240c7f3e8cc305271de942a1c
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-03 16:55:43 +0530

    Time: 710 ms (71.41%), Space: 0B (100.00%)

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select person.firstName , person.lastName, address.city, address.state from Person person left JOIN Address address on address.personId = person.personId;
