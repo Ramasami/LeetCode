c08f31bb11ae85b1f7f97cccbfe434b02c21c126
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-03 14:39:56 +0530

    Time: 1 ms (99.81%), Space: 50.6 MB (33.17%)

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,35 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int a = 0;
-        int b = 0;
         int ca = 0;
-        int cb = 0;
         for(int i=0;i<nums.length;i++) {
-            if(cb == 0) {
-                b = nums[i];
-                cb = 1;
-            } else if(ca == 0) {
+            if(ca == 0) {
                 a = nums[i];
                 ca = 1;
-            } else if (b == nums[i]) {
-                cb++;   
             } else if (a == nums[i]) {
                 ca++;
             } else {
-                cb--;
                 ca--;
             }
         }
-        if(ca==0)
-            return b;
-        ca = 0;
-        cb = 0;
-        for(int i=0;i<nums.length;i++) {
-            if (a==nums[i])   
-                ca++;
-            else if(b == nums[i])
-                cb++;
-        }
-        return ca>cb?a:b;
+        return a;
     }
 }
