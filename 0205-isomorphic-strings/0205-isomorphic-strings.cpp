692667b1eaf26be0753513211f25437d8fca9541
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-05 23:18:58 +0530

    Time: 6 ms (64.22%), Space: 8 MB (34.54%)

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,8 +3,8 @@ public:
     bool isIsomorphic(string s, string t) {
         if (s.length()!=t.length())
             return false;
-        int taken[257];
-        int map[257];
+        int taken[128];
+        int map[128];
         for (int i=0;i<s.length();i++) {
             int y = t[i] + 1;
             int x = s[i] + 1;
