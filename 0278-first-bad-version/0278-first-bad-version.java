5e40adb3ead364b1db0a3ef09f61c01a6989de79
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-06 02:12:17 +0530

    Time: 24 ms (84.99%), Space: 40.5 MB (27.24%)

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,7 +3,7 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int l = 0, r = n;
+        int l = 1, r = n;
         while(l<r) {
             int m = l + (r-l)/2;
             if(isBadVersion(m))
