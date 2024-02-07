341cbc0430341f012175a118157004c0aa089e14
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-07 23:41:18 +0530

    Time: 11 ms (38.73%), Space: 10.5 MB (18.50%)

---
+++ 0422-valid-word-square/0422-valid-word-square.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool validWordSquare(vector<string>& words) {
+        int l = words.size();
+        for(int i=0;i<l;i++) {
+            for(int j=0;j<words[i].length();j++) {
+                if (i >= words.size() || j >= words.size() || j >= words[i].length() || i >= words[j].length()) {
+                    return false;
+                }
+                if (words[i][j] != words[j][i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
