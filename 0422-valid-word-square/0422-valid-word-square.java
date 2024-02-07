99e23695f4f7fa93c5a516634b9633daa89d7e6a
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-07 23:43:38 +0530

    Time: 18 ms (15.21%), Space: 42.2 MB (57.03%)

--- 0422-valid-word-square/0422-valid-word-square.java
+++ 0422-valid-word-square/0422-valid-word-square.java
@@ -3,7 +3,7 @@ class Solution {
         int l = words.size();
         for(int i=0;i<l;i++) {
             for(int j=0;j<words.get(i).length();j++) {
-                if (j >= words.size() || j >= words.get(i).length() || i >= words.get(j).length()) {
+                if (j >= l || j >= words.get(i).length() || i >= words.get(j).length()) {
                     return false;
                 }
                 if (words.get(i).charAt(j) != words.get(j).charAt(i)) {
