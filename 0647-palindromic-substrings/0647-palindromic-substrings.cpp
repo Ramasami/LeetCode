cd0eabc5326c7370b371b95149d5d5d2b3ede36d
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-11 10:26:53 +0530

    Time: 4 ms (76.16%), Space: 7.5 MB (74.44%)

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -4,20 +4,18 @@ public:
         int n = s.length();
         int ans = 0;
         for(int i=0;i<n;i++) {
-            bool sing = true;
-            bool doub = true;
-            int m = min(i+1,n-i+1);
+            int m = min(i+1,n-i);
             for(int j=0;j<m;j++) {
-                
-                if(sing && s[i-j] == s[i+j])
+                if(s[i-j] == s[i+j])
                     ans++;
                 else
-                    sing = false;
-                if(doub && s[i-j] == s[i+j+1])
+                    break;
+            }
+            m = min(i+1,n-i+1);
+            for(int j=0;j<m;j++) {
+                if(s[i-j] == s[i+j+1])
                     ans++;
                 else
-                    doub = false;
-                if(!sing && !doub)
                     break;
             }
         }
