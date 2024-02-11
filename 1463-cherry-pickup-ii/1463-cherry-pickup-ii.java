d3913d56fbe713e0792c55fb77a1ef8fcc8a5366
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-11 11:44:33 +0530

    Time: 26 ms (58.94%), Space: 46 MB (48.79%)

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -9,7 +9,6 @@ class Solution {
             }
         }
         dp[0][0][m-1] = grid[0][0] + grid[0][m-1];
-        // printGrid(dp[0],m);
         for(int i=1;i<n;i++) {
             for(int r1=0;r1<m;r1++) {
                 for(int r2=0;r2<m;r2++) {
@@ -31,20 +30,8 @@ class Solution {
                         dp[i][r1][r2] = -1;
                 }
             }
-            // printGrid(dp[i],m);
         }
         return getMax(dp[n-1], m);
-    }
-    
-    private void printGrid(int[][] grid, int m) {
-        System.out.println("##########");
-            for(int i=0;i<m;i++) {
-                for(int j=0;j<m;j++) {
-                    System.out.print(grid[i][j]+" ");
-                }
-                System.out.println();
-            }
-            System.out.println();
     }
     
     private int getMax(int[][] grid, int m) {
