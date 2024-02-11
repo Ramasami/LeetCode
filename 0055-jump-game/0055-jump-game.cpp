29d002e64e544bb96c99ebd0a670a2bf33f0cb81
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-11 16:54:11 +0530

    Time: 35 ms (96.30%), Space: 50.7 MB (45.35%)

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        nums[n-1] = 1;
         int i = 0;
-        int x = abs(nums[i]);
-        while(x>=0 & i < n) {
-            if(abs(nums[i]) > x) {
-                x = abs(nums[i]);
+        int x = nums[i];
+        while(x>=0 && i < n) {
+            if(nums[i] > x) {
+                x = nums[i];
             }
-            nums[i] = -abs(nums[i]);
             i++;
             x--;
         }
-        return nums[n-1] < 0;
+        return i>=n;
     }
 };
