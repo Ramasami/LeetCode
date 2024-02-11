929c75c116f472e0ff189dad7fa5c36a0c43f143
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-11 19:12:14 +0530

    Time: 664 ms (5.01%), Space: 19 MB (15.29%)

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -6,9 +6,12 @@ public:
         for(int i=1;i<n;i++)
             dp[i] = 100000;
         dp[0] = 0;
+        int max = 0;
         for(int i=0;i<n;i++) {
-            for(int j=1;j<=nums[i]&&i+j<n;j++) {
-                dp[i+j] = min(dp[i+j], dp[i]+1);
+            if(dp[i]+i<max)
+                continue;
+            for(int j=max;j<=nums[i]+i&&j<n;j++) {
+                dp[j] = min(dp[j], dp[i]+1);
             }
         }
         return dp[n-1];
