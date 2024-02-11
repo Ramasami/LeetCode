fe9ece1da90bfb0190bd2c3aa193dd7603cc13ca
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-11 16:53:10 +0530

    Time: 3 ms (58.75%), Space: 45.6 MB (43.30%)

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        nums[n-1] = 1;
         int i = 0;
         int x = nums[i];
         while(x>=0 & i < n) {
