8243757f0d31f4edeaf9a21fd6b51a9d1acbf963
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-11 12:41:26 +0530

    Time: 96 ms (9.35%), Space: 12.1 MB (23.99%)

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> set;
+        int l = 0;
+        int r = 0;
+        int n = s.length();
+        int ans = 0;
+        for(r=0;r<n;r++) {
+            char c = s[r];
+            if(set.find(c) != set.end()) {
+                while(s[l]!=c) {
+                    set.erase(s[l]);
+                    l++;
+                }
+                l++;
+            }
+            set.insert(c);
+            
+            cout<<l<<" "<<r<<" " <<s[l]<<" "<<s[r]<<"\n";
+            ans = max(ans, r-l+1);
+        }
+        return ans;
+    }
+};
