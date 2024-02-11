8fdd168e739c7c3e3a32786043a3cd507b055daf
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-11 19:15:34 +0530

    Time: 39 ms (37.51%), Space: 19 MB (24.84%)

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -8,12 +8,18 @@ public:
         dp[0] = 0;
         int max = 0;
         for(int i=0;i<n;i++) {
-            if(dp[i]+i<max)
+            if(nums[i]+i<max) {
                 continue;
+            }
             for(int j=max;j<=nums[i]+i&&j<n;j++) {
                 dp[j] = min(dp[j], dp[i]+1);
+                max = j;
             }
         }
+         for(int i=0;i<n;i++) {
+            cout<<dp[i]<<" ";   
+        }
+        cout<<"\n";
         return dp[n-1];
     }
 };
