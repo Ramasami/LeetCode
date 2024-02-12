1ddee50a29f9abd8afb9b8507e94494f5e90cf75
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-12 23:18:42 +0530

    Time: 18 ms (85.02%), Space: 44.9 MB (22.78%)

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -6,23 +6,16 @@ class Solution {
             Map<Double, Integer> angleCounter = new HashMap<>();
             int x1 = points[i][0];
             int y1 = points[i][1];
-            int xAxis = 1;
             for(int j=0;j<n;j++) {
                 if(i==j)
                     continue;
                 int x2 = points[j][0];
                 int y2 = points[j][1];
-                if(x1==x2) {
-                    xAxis++;
-                }
-                else {
-                    double angle =(y1-y2)/(0.0+x1-x2);
-                    angleCounter.compute(angle, (k,v)->v==null?2:v+1);
-                }
+                double angle =(y1-y2)/(0.0+x1-x2);
+                angleCounter.compute(angle, (k,v)->v==null?2:v+1);
             }
             for(int count: angleCounter.values())
-                xAxis = Math.max(xAxis, count);
-            max = Math.max(max, xAxis);
+                max = Math.max(max, count);
         }
         return max;
     }
