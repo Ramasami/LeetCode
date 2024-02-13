98ad1042dc263f83123bbf8da5b429c744919fce
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-13 20:10:33 +0530

    Time: 4 ms (40.56%), Space: 44.9 MB (65.40%)

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String word:words) {
+            if (word.equals(new StringBuilder(word).reverse().toString()))
+                return word;
+        }
+        return "";
+    }
+}
