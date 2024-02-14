c585882bbdf1df15e0b43a1c36bef236da74a45e
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-14 18:46:12 +0530

    Time: 678 ms (68.65%), Space: 0B (100.00%)

--- 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
+++ 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
@@ -1,2 +1,9 @@
 # Write your MySQL query statement below
-select distinct(c1.seat_id) from Cinema c1 Join Cinema c2 where c1.free = 1 and c2.free=1 and abs(c1.seat_id - c2.seat_id) = 1 order by c1.seat_id;
+select 
+distinct(c1.seat_id) 
+from Cinema c1 
+Join Cinema c2 
+on abs(c1.seat_id-c2.seat_id) = 1
+where 
+c1.free = 1 and c2.free=1
+order by c1.seat_id;
