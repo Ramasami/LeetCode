4ef7578717dde0102d328ce3681293f42bcf8b21
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-18 01:07:29 +0530

    Time: 72 ms (80.96%), Space: 57.2 MB (55.14%)

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int,vector<int>, greater<int>> ladderAllocations;
+        for (int i = 0; i < heights.size() - 1; i++) {
+            int climb = heights[i + 1] - heights[i];
+            if (climb <= 0) {
+                continue;
+            }
+            ladderAllocations.push(climb);
+            if (ladderAllocations.size() <= ladders) {
+                continue;
+            }
+            bricks -= ladderAllocations.top();
+            ladderAllocations.pop();
+            if (bricks < 0) {
+                return i;
+            }
+        }
+        return heights.size() - 1;
+    }
+};
