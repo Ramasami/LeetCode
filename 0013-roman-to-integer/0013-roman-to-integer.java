f5c9e495b2bab208754775d829aaa0141dd78b8b
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-19 02:21:56 +0530

    Time: 2 ms (100.00%), Space: 44.3 MB (92.94%)

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int romanToInt(String s) {
+        int ans = 0;
+        char prev = 0;
+        for (int i=0;i<s.length();i++) {
+            switch (s.charAt(i)) {
+                case 'M': {
+                    if (prev == 'C')
+                        ans+=900 - 100;
+                    else
+                        ans+=1000;
+                    break;
+                    }
+                case 'D': {
+                    if (prev == 'C')
+                        ans+=400 - 100;
+                    else
+                        ans+=500;
+                    break;
+                    }
+                case 'C': {
+                    if (prev == 'X')
+                        ans+=90 - 10;
+                    else
+                        ans+=100;
+                    break;
+                    }
+                case 'L': {
+                    if (prev == 'X')
+                        ans+=40 -10;
+                    else
+                        ans+=50;
+                    break;
+                    }
+                case 'X': {
+                    if (prev == 'I')
+                        ans+=9 - 1;
+                    else
+                        ans+=10;
+                    break;
+                    }
+                case 'V': {
+                    if (prev == 'I')
+                        ans+=4 - 1;
+                    else
+                        ans+=5;
+                    break;
+                    }
+                case 'I': {
+                    {
+                        ans+=1;
+                    break;
+                    }
+                }
+            }
+            prev = s.charAt(i);
+        }
+        return ans;
+    }
+}
