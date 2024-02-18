a50e05993cd525e6e1970ed8202162ecbd8396f8
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-19 00:25:31 +0530

    Time: 79 ms (45.53%), Space: 97.3 MB (92.74%)

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int mostBooked(int n, int[][] meetings) {
+        int[] meetingCount = new int[n];
+        Queue<Integer> unusedRoom = new PriorityQueue<>();
+        Queue<long[]> usedRoom = new PriorityQueue<>(
+            (a,b) -> 
+            a[0]!=b[0] ? 
+            Long.compare(a[0],b[0])
+            :Long.compare(a[1],b[1])
+        );
+        
+        for(int i=0;i<n;i++)
+            unusedRoom.add(i);
+        
+        Arrays.sort(meetings, (a,b)->a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+        
+        for(int[] meeting:meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
+            while(!usedRoom.isEmpty() && usedRoom.peek()[0] <= start) {
+                int room = (int) usedRoom.poll()[1];
+                unusedRoom.add(room);
+            }
+            
+            if(!unusedRoom.isEmpty()) {
+                int room = unusedRoom.poll();
+                usedRoom.add(new long[]{end, room});
+                meetingCount[room]++;
+            } else {
+                long roomAvailabilityTime = usedRoom.peek()[0];
+                int room = (int) usedRoom.poll()[1];
+                usedRoom.add(new long[]{roomAvailabilityTime + end - start, room});
+                meetingCount[room]++;
+            }
+        }
+        
+        int maxMeetingCount = 0, maxMeetingCountRoom = 0;
+        for (int i = 0; i < n; i++) {
+            if (meetingCount[i] > maxMeetingCount) {
+                maxMeetingCount = meetingCount[i];
+                maxMeetingCountRoom = i;
+            }
+        }
+
+        return maxMeetingCountRoom;
+    }
+    
+    private void print(Queue<Pair<Integer, Integer>> pq) {
+        for(Pair<Integer,Integer> p:pq) {
+            System.out.println(p);
+        }
+        System.out.println("###");
+    }
+}
