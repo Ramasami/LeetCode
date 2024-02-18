6b48df9e2e6e17c0494b12e4c7cbe990e98beba1
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-19 02:14:46 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (99.93%)

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int totalMoney(int n) {
+        int w = n/7;
+        int ans = 28 * w + w * (w-1) * 7 / 2;
+        int d = n%7;
+        ans+=d * (2*(w+1) + (d-1)) / 2;
+        return ans;
+    }
+}
