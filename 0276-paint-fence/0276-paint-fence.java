d41e9e6e83edd9232d709493e57a674e891bd109
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-19 01:40:19 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (36.03%)

---
+++ 0276-paint-fence/0276-paint-fence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numWays(int n, int k) {
+        if(n==1)
+            return k;
+        if(n==2)
+            return k*k;
+        int n0=0;
+        int n1=k*k;
+        int n2=k;
+        for(int i=3;i<=n;i++) {
+            n0 = (k-1)*(n1+n2);
+            n2 = n1;
+            n1 = n0;
+        }
+        
+        return n0;
+    }
+    
+}
