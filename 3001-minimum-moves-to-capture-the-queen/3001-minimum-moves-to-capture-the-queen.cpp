bc8de7d07d70a324e34451d0d3528a037d623dfe
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-19 02:10:29 +0530

    Time: 2 ms (41.87%), Space: 7.8 MB (6.13%)

---
+++ 3001-minimum-moves-to-capture-the-queen/3001-minimum-moves-to-capture-the-queen.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
+        if (a == e) {   
+            if (c == a) {
+                if (b < f && b < d && d < f) return 2;
+                if (b > f && b > d && d > f) return 2;
+            }
+            return 1;
+        }
+        if (b == f) {
+            if (d == f) {
+                if (a > e && a > c && c > e) return 2;
+                if (a < e && a < c && c < e) return 2;
+            }
+            return 1;
+        }
+        if ((e-f) == (c-d)) {   
+            if ((e-f) == (a-b)) {
+                if (c < e && c < a && a < e) return 2;
+                if (c > e && c > a && a > e) return 2;
+            }
+            return 1;
+        }
+        if ((e+f) == (c+d)) {
+            if ((a+b) == (e+f)) {
+                if (c < e && c < a && a < e) return 2;
+                if (c > e && c > a && a > e) return 2;
+            }
+            return 1;
+        }
+        return 2;
+    }
+};
