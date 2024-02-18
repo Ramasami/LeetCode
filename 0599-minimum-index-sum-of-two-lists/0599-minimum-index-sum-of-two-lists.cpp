52fbebe274ba01e7cadd6cd556a7491f26918dd4
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-19 01:05:34 +0530

    Time: 69 ms (78.05%), Space: 47.1 MB (5.34%)

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -4,8 +4,8 @@ public:
         int i=0, count = 0,min = INT_MAX;
         vector<string> *ans = new vector<string>();
         unordered_map<string, int> map;
-        vector<string> m = list1.size() >= list2.size() ? list1 : list2;
-        vector<string> list = list1.size() < list2.size() ? list1 : list2;
+        vector<string> m = list1.size() < list2.size() ? list1 : list2;
+        vector<string> list = list1.size() >= list2.size() ? list1 : list2;
         for(string s:m) {
             if(map.count(s) == 0)
                 map[s]=i;
