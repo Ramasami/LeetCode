bb76fc3d8318ad9efee6930994722d739eb4cae1
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-20 01:43:36 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (33.49%)

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int t;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<i;j++) {
+                t = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = t;
+            }
+        }
+        
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n/2;j++) {
+                t = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = t;
+            }
+        }
+    }
+};
