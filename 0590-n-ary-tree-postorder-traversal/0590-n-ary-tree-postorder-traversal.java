2ceef9447a7d5ca4b4e594f619206a3bb21b2eee
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-20 21:47:47 +0530

    Time: 3 ms (23.44%), Space: 45 MB (10.65%)

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.java
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.java
@@ -19,22 +19,16 @@ class Node {
 
 class Solution {
     public List<Integer> postorder(Node root) {
-        List<Integer> ans = new ArrayList<>();
+        List<Integer> ans = new LinkedList<>();
         if(root==null)
             return ans;
-        Set<Node> visited = new HashSet<>();
-        Stack<Node> st = new Stack<>();
+        LinkedList<Node> st = new LinkedList<>();
         st.push(root);
         while(!st.isEmpty()) {
-            Node top = st.peek();
-            if(visited.contains(top)) {
-                ans.add(top.val);
-                st.pop();
-            } else {
-                for(int i=top.children.size()-1;i>=0;i--)
-                    st.push(top.children.get(i));
-                visited.add(top);
-            }
+            Node top = st.pollLast();
+            ans.addFirst(top.val);
+            for(Node n:top.children)
+                st.add(n);
         }
         return ans;
     }
