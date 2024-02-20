a52053eb555d104ce176fc84c833a14892e23432
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-20 20:23:43 +0530

    Time: 4 ms (62.56%), Space: 58.6 MB (5.21%)

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.java
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.java
@@ -1,11 +1,10 @@
 class Solution {
     public int maxDistance(int[] nums1, int[] nums2) {
         int l = 0, r = 0;
-        int n = nums1.length, m = nums2.length;
         int ans = 0;
-        while(l<n) {
+        while(l<nums1.length) {
             int x = nums1[l];
-            while(r<m) {
+            while(r<nums2.length) {
                 if(nums2[r]<x)
                     break;
                 r++;
