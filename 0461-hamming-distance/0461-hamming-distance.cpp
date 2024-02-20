5410af98ed80a79c2ae2a3d7943bf019e4cb21bf
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-20 22:40:35 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (28.79%)

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -3,7 +3,7 @@ public:
     int hammingDistance(int x, int y) {
         x = x ^ y;
         int ans = 0;
-        while(x>0) {
+        while(x!=0) {
             x=x&(x-1);
             ans++;
         }
