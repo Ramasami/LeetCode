1ccbe5e25758dcad2e91e48075190292135bf353
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-20 20:21:11 +0530

    Time: 308 ms (7.81%), Space: 100.8 MB (44.90%)

---
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int l = 0, r = 0;
+        int n = nums1.size(), m = nums2.size();
+        int ans = 0;
+        while(l<n) {
+            int x = nums1[l];
+            while(r<m) {
+                if(nums2[r]<x)
+                    break;
+                r++;
+                cout<<l<<" "<<r<<"\n";
+            }
+            ans = max(ans, r-l-1);
+            l++;
+        }
+        return ans;
+    }
+};
