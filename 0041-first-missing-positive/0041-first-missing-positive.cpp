802400165f7d40563f2d3fd70a991e18a0c15054
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-21 23:02:50 +0530

    Time: 43 ms (75.69%), Space: 43.4 MB (61.21%)

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int c = 10002;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+            if(nums[i] == 0)
+                nums[i] = c;
+        for(int i=0;i<n;i++) {
+            if(nums[i]<=n && nums[i] > 0) {
+                parse(nums, nums[i]-1, n);
+            }
+        }
+        for(int i=0;i<n;i++)
+            if(nums[i]!=0)
+                return i+1;
+        return n+1;
+    }
+    
+    void parse(vector<int>& nums, int i, int n) {
+        int t = nums[i];
+        nums[i] = 0;
+        if(t<=n && t>0)
+            parse(nums, t-1, n);
+    }
+    
+    void print(vector<int> nums) {
+         for(int a: nums) {
+                cout<<a<<" ";
+            }
+            cout<<"\n";
+    }
+};
