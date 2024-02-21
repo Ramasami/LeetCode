3598e8b1a279b71e7854aab59ec5c288c5269d2a
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-21 22:47:47 +0530

    Time: 3 ms (99.51%), Space: 33.8 MB (5.20%)

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -3,14 +3,15 @@ public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
         int count[201]={0};
         vector<vector<int>> ans;
+        int i,j;
         for(int n:nums) {
             count[n]++;
             if(ans.size()<count[n])
                 ans.push_back(vector<int>());
         }
         
-        for(int i=1;i<=200;i++) {
-            for(int j=0;j<count[i];j++)
+        for(i=1;i<=200;i++) {
+            for(j=0;j<count[i];j++)
                 ans[j].push_back(i);
         }
         return ans;
