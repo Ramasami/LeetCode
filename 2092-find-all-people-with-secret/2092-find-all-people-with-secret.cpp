67166a53e34deec37e4701668a7a122d80d7e614
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-02-25 00:55:54 +0530

    Time: 414 ms (86.07%), Space: 184.4 MB (48.36%)

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,49 +1,48 @@
 class Solution {
-    public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+public:
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
         // For every person, we store the meeting time and label of the person met.
-        Map<Integer, List<int[]>> graph = new HashMap<>();
-        for (int[] meeting : meetings) {
+        unordered_map<int, vector<pair<int, int>>> graph;
+        for (auto& meeting : meetings) {
             int x = meeting[0], y = meeting[1], t = meeting[2];
-            graph.computeIfAbsent(x, k -> new ArrayList<>()).add(new int[]{t, y});
-            graph.computeIfAbsent(y, k -> new ArrayList<>()).add(new int[]{t, x});
+            graph[x].emplace_back(t, y);
+            graph[y].emplace_back(t, x);
         }
 
-        // Earliest time at which a person learned the secret 
-        // as per current state of knowledge. If due to some new information, 
+        // Earliest time at which a person learned the secret
+        // as per current state of knowledge. If due to some new information,
         // the earliest time of knowing the secret changes, we will update it
         // and again process all the people whom he/she meets after the time
         // at which he/she learned the secret.
-        int[] earliest = new int[n];
-        Arrays.fill(earliest, Integer.MAX_VALUE);
+        vector<int> earliest(n, INT_MAX);
         earliest[0] = 0;
         earliest[firstPerson] = 0;
-        
+
         // Queue for BFS. It will store (person, time of knowing the secret)
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0});
-        q.offer(new int[]{firstPerson, 0});
+        queue<pair<int, int>> q;
+        q.emplace(0, 0);
+        q.emplace(firstPerson, 0);
 
         // Do BFS
-        while (!q.isEmpty()) {
-            int[] personTime = q.poll();
-            int person = personTime[0], time = personTime[1];
-            for (int[] nextPersonTime : graph.getOrDefault(person, new ArrayList<>())) {
-                int t = nextPersonTime[0], nextPerson = nextPersonTime[1];
+        while (!q.empty()) {
+            auto [person, time] = q.front();
+            q.pop();
+            for (auto [t, nextPerson] : graph[person]) {
                 if (t >= time && earliest[nextPerson] > t) {
                     earliest[nextPerson] = t;
-                    q.offer(new int[]{nextPerson, t});
+                    q.emplace(nextPerson, t);
                 }
             }
         }
-        
+
         // Since we visited only those people who know the secret,
         // we need to return indices of all visited people.
-        List<Integer> ans = new ArrayList<>();
+        vector<int> ans;
         for (int i = 0; i < n; ++i) {
-            if (earliest[i] != Integer.MAX_VALUE) {
-                ans.add(i);
+            if (earliest[i] != INT_MAX) {
+                ans.push_back(i);
             }
         }
         return ans;
     }
-}
+};
