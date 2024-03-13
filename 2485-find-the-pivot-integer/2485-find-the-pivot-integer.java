c774cd50954b6d57cb8c1568e11876a0ced1b45b
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-03-13 21:59:47 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (17.72%)

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int a = n * n + n;
+        if (a%2==1)
+            return -1;
+        int pivot = (int)Math.sqrt(a/2);
+        return pivot * pivot == a / 2? pivot:-1;
+    }
+}
