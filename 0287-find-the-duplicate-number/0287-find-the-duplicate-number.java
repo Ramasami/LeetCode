59fd39d7248fbc54c5d35353411add71bdb5ce96
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-03-24 08:43:34 +0530

    Time: 5 ms (68.61%), Space: 57.9 MB (68.90%)

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,12 +1,17 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        int ans = -1;
         for(int i:nums) {
             int x = Math.abs(i);
-            if(nums[x]<0)
-                return x;
+            if(nums[x]<0) {
+                ans = x;
+                break;
+            }
             else
                 nums[x]*=-1;
         }
-        return -1;
+        for (int i = 0; i < nums.length; i++)
+            nums[i] = Math.abs(nums[i]);
+        return ans;
     }
 }
