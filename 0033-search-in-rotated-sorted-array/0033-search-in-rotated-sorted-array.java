066d2da019526db4d2c23da30aab03396f5da3fa
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-04-01 23:44:22 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (50.78%)

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int pivot = findPivot(nums, 0, nums.length-1,nums.length);
+        // System.out.println(nums[pivot]); 
+        
+        if(pivot!=0 && target >= nums[0] && target <=nums[pivot-1])
+            return find(nums, target, 0, pivot);
+        else if(target >= nums[pivot] && target <=nums[nums.length-1])
+            return find(nums, target, pivot, nums.length);
+        else 
+            return -1;
+    }
+    
+    private int find(int[] nums, int t, int l, int r) {
+        while(l<r) {
+            int m = l + (r-l)/2;
+            // System.out.println(nums[l] + " " + nums[r-1] + " " + nums[m]); 
+            if(nums[m]>t)
+                r = m;
+            else if(nums[m]<t) 
+                l = m+1;
+            else 
+                return m;
+        }
+        return -1;
+    }
+    
+    private int findPivot(int[] nums, int l, int r,int n) {
+        if(l>r)
+            return l;
+        int m = l + (r-l)/2;
+        // System.out.println(nums[l] + " " + nums[r] + " " + nums[m]); 
+        if(nums[n-1]<nums[m]) {
+            return findPivot(nums, m+1, r, n);
+        } else {
+            return findPivot(nums, l, m-1, n);
+        }
+    }
+}
