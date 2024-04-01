877eef2f944cf791122533e99f27d2b05bd5b2e2
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-04-01 22:52:40 +0530

    Time: 2 ms (49.51%), Space: 44.2 MB (76.83%)

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,24 +2,25 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-        permute(nums,0,n,ans);
+        List<Integer> numbers = new ArrayList<>(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+        permute(numbers,0,n,ans);
         return ans;
     }
     
-    private void permute(int[] nums, int l, int r, List<List<Integer>> ans) {
+    private void permute(List<Integer> numbers, int l, int r, List<List<Integer>> ans) {
         if(l == r) {
-            ans.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+            ans.add(new ArrayList<>(numbers));
         }
         for(int i=l;i<r;i++) {
-            swap(nums,l,i);
-            permute(nums,l+1,r,ans);
-            swap(nums,l,i);
+            swap(numbers,l,i);
+            permute(numbers,l+1,r,ans);
+            swap(numbers,l,i);
         }
     }
     
-    private void swap(int[] nums, int i, int j) {
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
+    private void swap(List<Integer> numbers, int i, int j) {
+        int t = numbers.get(i);
+        numbers.set(i, numbers.get(j));
+        numbers.set(j, t);
     }
 }
