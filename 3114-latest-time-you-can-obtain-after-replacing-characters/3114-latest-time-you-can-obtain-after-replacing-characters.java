e51d9e91be0da1be5f7baea7f01dc32b52f65307
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-04-20 10:13:22 +0530

    Time: 14 ms (5.36%), Space: 45.3 MB (5.16%)

--- 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.java
+++ 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.java
@@ -3,23 +3,26 @@ class Solution {
         String ans = null;
         if(s.charAt(0) == '?' && s.charAt(1) == '?') {
             ans="11:";
-        } else if(s.charAt(0) == '?' && s.charAt(1) <= '1') {
-            ans="1"+s.charAt(1)+":";
-        } else if(s.charAt(0) == '?' && s.charAt(1) > '1') {
-            ans="0"+s.charAt(1)+":";
-        } else if(s.charAt(0) == '0' && s.charAt(1) == '?') {
-            ans=s.charAt(0)+"9:";
-        } else if(s.charAt(0) == '1' && s.charAt(1) == '?') {
-            ans="11:";
+        } else if(s.charAt(0) == '?') {
+            if(s.charAt(1) <= '1') {
+                ans="1"+s.charAt(1)+":";
+            } else {
+                ans="0"+s.charAt(1)+":";
+            }
+        } else if(s.charAt(1) == '?') {
+            if(s.charAt(0) == '0')
+                ans=s.charAt(0)+"9:";
+            else
+                ans="11:";
         } else {
             ans=s.charAt(0) + "" + s.charAt(1) + ":";
         }
         
         if(s.charAt(3) == '?' && s.charAt(4) == '?') {
             ans+="59";
-        } else if(s.charAt(3) == '?' && s.charAt(4) != '?') {
+        } else if(s.charAt(3) == '?') {
             ans+="5"+s.charAt(4);
-        } else if(s.charAt(3) != '?' && s.charAt(4) == '?') {
+        } else if(s.charAt(4) == '?') {
             ans+=s.charAt(3)+"9";
         } else {
             ans+=s.charAt(3) + "" + s.charAt(4);
