dda552fbb0f9a276e7c62bbf284cf5b9332a805e
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-04-20 10:10:32 +0530

    Time: 15 ms (5.36%), Space: 43.6 MB (13.91%)

---
+++ 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String findLatestTime(String s) {
+        String ans = null;
+        if(s.charAt(0) == '?' && s.charAt(1) == '?') {
+            ans="11:";
+        } else if(s.charAt(0) == '?' && s.charAt(1) <= '1') {
+            ans="1"+s.charAt(1)+":";
+        } else if(s.charAt(0) == '?' && s.charAt(1) > '1') {
+            ans="0"+s.charAt(1)+":";
+        } else if(s.charAt(0) == '0' && s.charAt(1) == '?') {
+            ans=s.charAt(0)+"9:";
+        } else if(s.charAt(0) == '1' && s.charAt(1) == '?') {
+            ans="11:";
+        } else {
+            ans=s.charAt(0) + "" + s.charAt(1) + ":";
+        }
+        
+        if(s.charAt(3) == '?' && s.charAt(4) == '?') {
+            ans+="59";
+        } else if(s.charAt(3) == '?' && s.charAt(4) != '?') {
+            ans+="5"+s.charAt(4);
+        } else if(s.charAt(3) != '?' && s.charAt(4) == '?') {
+            ans+=s.charAt(3)+"9";
+        } else {
+            ans+=s.charAt(3) + "" + s.charAt(4);
+        }
+        return ans;
+    }
+}
