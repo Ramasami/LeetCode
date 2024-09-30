006c181a9f86be921112da7ce8e95415b49ce66b
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-09-30 11:51:42 +0530

    Time: 1 ms (100.00%), Space: 44.8 MB (28.17%)

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,31 +1,31 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         for(int i=0;i<9;i++) {
-            int[] v = new int[9];
-            int[] h = new int[9];
+            int v = 0;
+            int h = 0;
             for(int j=0;j<9;j++) {
                 if(board[i][j] != '.') {
-                    if(v[board[i][j]-'1'] == 1)
+                    if((v & 1<< (board[i][j]-1)) >0)
                         return false;
                     else
-                        v[board[i][j]-'1'] = 1;
+                        v = v | (1 << (board[i][j]-1));
                 }
                 if(board[j][i] != '.') {
-                    if(h[board[j][i]-'1'] == 1)
+                    if((h & 1<< (board[j][i]-1)) >0)
                         return false;
                     else
-                        h[board[j][i]-'1'] = 1;
+                        h = h | (1 << (board[j][i]-1));
                 }
                 
                 if(i%3==0 && j%3==0) {
-                    int[] b = new int[9];
+                    int b = 0;
                     for(int x = i;x<i+3;x++) {
                         for(int y = j;y<j+3;y++) {
                             if(board[x][y] != '.') {
-                                if(b[board[x][y]-'1'] == 1)
+                                if((b & 1<< (board[x][y]-1)) >0)
                                     return false;
                                 else
-                                    b[board[x][y]-'1'] = 1;
+                                    b = b | (1 << (board[x][y]-1));
                             }
                         }
                     }
