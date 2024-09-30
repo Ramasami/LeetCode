76341fade26cbe0e46ddf555fc2f247dee4402e8
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-09-30 12:16:23 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (9.42%)

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -11,7 +11,6 @@ class Solution {
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
                 int neighbour = getNeighbour(board, i, j);
-                System.out.println(i + " " + j + " " + neighbour);
                 int curr = board[i][j];
                 if (neighbour < 2 && curr == 1) {
                     board[i][j] = 3;
