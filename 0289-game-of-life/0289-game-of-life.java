915c5bfb158e37787f6a00172e882d6b613212b5
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-09-30 12:16:15 +0530

    Time: 14 ms (5.42%), Space: 42 MB (9.42%)

---
+++ 0289-game-of-life/0289-game-of-life.java
@@ -0,0 +1,58 @@
+class Solution {
+    
+    
+    // 0 dead
+    // 1 alive
+    // 2 was dead, now alive
+    // 3 was alive now dead
+    public void gameOfLife(int[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                int neighbour = getNeighbour(board, i, j);
+                System.out.println(i + " " + j + " " + neighbour);
+                int curr = board[i][j];
+                if (neighbour < 2 && curr == 1) {
+                    board[i][j] = 3;
+                } else if (neighbour == 3 && curr == 0) {
+                    board[i][j] = 2;
+                } else if (neighbour > 3 && curr == 1) {
+                    board[i][j] = 3;
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                 if (board[i][j] >= 2)
+                     board[i][j] = 3-board[i][j];
+            }
+        }
+    }
+    
+    private int getNeighbour(int[][] board, int x, int y) {
+        int n = board.length;
+        int m = board[0].length;
+        int ans = 0;
+        if (x != n-1) {
+            ans+=(board[x+1][y]%2);
+            if (y != m-1)
+                ans+=(board[x+1][y+1]%2);
+            if (y != 0)
+                ans+=(board[x+1][y-1]%2);
+        }
+        if (x != 0) {
+            ans+=(board[x-1][y]%2);
+            if (y != m-1)
+                ans+=(board[x-1][y+1]%2);
+            if (y != 0)
+                ans+=(board[x-1][y-1]%2);
+        }
+        if (y != m-1)
+            ans+=(board[x][y+1]%2);
+        if (y != 0)
+            ans+=(board[x][y-1]%2);
+        return ans;
+    }
+}
