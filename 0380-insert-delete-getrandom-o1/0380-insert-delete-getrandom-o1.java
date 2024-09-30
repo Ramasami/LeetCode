21cc839bb93bb285d3e5178a262273399a9b18b1
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-09-30 09:45:45 +0530

    Time: 26 ms (62.92%), Space: 92.6 MB (46.79%)

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,24 +1,38 @@
 class RandomizedSet {
     
     private Random rand;
-    private Set<Integer> numbers;
+    private Map<Integer, Integer> index;
+    private List<Integer> numbers;
 
     public RandomizedSet() {
         rand = new Random();
-        numbers = new HashSet<>();
+        numbers = new ArrayList<>();
+        index = new HashMap<>();
     }
     
     public boolean insert(int val) {
-        return numbers.add(val);
+        if (index.containsKey(val))
+            return false;
+        index.put(val, numbers.size());
+        numbers.add(numbers.size(), val);
+        return true;
     }
     
     public boolean remove(int val) {
-        return numbers.remove(val);
+        if (!index.containsKey(val))
+            return false;
+        
+        int lastElement = numbers.get(numbers.size() - 1);
+        int idx = index.get(val);
+        numbers.set(idx, lastElement);
+        index.put(lastElement, idx);
+        numbers.remove(numbers.size() - 1);
+        index.remove(val);
+        return true;
     }
     
     public int getRandom() {
-        int i = rand.nextInt(numbers.size());
-        return (int)numbers.toArray()[i];
+        return numbers.get(rand.nextInt(numbers.size()));
     }
 }
 
