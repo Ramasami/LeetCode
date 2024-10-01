53d20881b9617679cb584f8159718a3ce5d90784
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-10-01 17:55:24 +0530

    Time: 8 ms (22.47%), Space: 43.3 MB (51.51%)

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,61 +1,22 @@
 class Solution {
     public String simplifyPath(String path) {
-        LinkedList<String> ll = new LinkedList<String>();
-        ll.push("/");
-        path+="/";
-        for(char c: path.toCharArray()) {
-            String top = ll.peek();
-            switch (c) {
-                case '/': {
-                    switch (top) {
-                        case "/":break;
-                        case ".":ll.pop(); break;
-                        case "..": {
-                            ll.pop();
-                            if (ll.size()>2) {
-                                ll.pop();
-                                ll.pop(); 
-                            }
-                            break;
-                        }
-                        default:ll.push("/");
-                    } 
-                    break;
-                } 
-                case '.': {
-                    switch (top) {
-                        case "/": {
-                            ll.push(".");
-                            break;
-                        }
-                        default: {
-                            ll.pop();
-                            ll.push(top+".");
-                        }
-                    }
-                    break;
+        Stack<String> stack = new Stack<String>();
+        String[] components = path.split("/");
+
+        for (String directory : components) {
+            if (directory.equals(".") || directory.isEmpty()) {
+                continue;
+            } else if (directory.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
                 }
-                default: {
-                    switch(top) {
-                        case "/": {
-                            ll.push(String.valueOf(c));
-                            break;
-                        } 
-                        default :{
-                            ll.pop();
-                            ll.push(top+c);
-                        }
-                    }
-                }
+            } else {
+                stack.add(directory);
             }
         }
-        if (ll.size() > 1 && ll.peek().equals("/"))
-            ll.pop();
-        
-        StringBuilder ans = new StringBuilder();
-        while(!ll.isEmpty()) {
-            ans.append(ll.pollLast());
-        }
-        return ans.toString();
+
+    
+
+        return "/" + stack.stream().collect(Collectors.joining("/"));
     }
 }
