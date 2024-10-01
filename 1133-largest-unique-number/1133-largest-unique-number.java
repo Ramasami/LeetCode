46c324696fd2b08cac68e44f172201295e38ede3
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-10-01 07:56:00 +0530

    Time: 1 ms (99.20%), Space: 43 MB (51.06%)

--- 1133-largest-unique-number/1133-largest-unique-number.java
+++ 1133-largest-unique-number/1133-largest-unique-number.java
@@ -1,23 +1,13 @@
 class Solution {
     public int largestUniqueNumber(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue((o1,o2)->((int)o2-(int)o1));
-        int[] v = new int[1001];
-        int[] nv = new int[1001];
+        int[] c = new int[1001];
         for(int i: nums) {
-            if(v[i]==0) {
-                if(nv[i] == 1) {
-                    pq.remove(i);
-                    v[i] = 1;
-                    nv[i] = 0;
-                } else {
-                    nv[i] = 1;
-                    pq.add(i);
-                }
-            }
+            c[i]++;
         }
-        if(pq.isEmpty())
-            return -1;
-        else
-            return pq.poll();
+        for(int i=1000;i>=0;i--) {
+            if(c[i]==1)
+                return i;
+        }
+        return -1;
     }
 }
