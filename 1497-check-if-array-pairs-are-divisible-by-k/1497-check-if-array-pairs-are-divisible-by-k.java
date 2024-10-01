e6c5326fa08e0b12af94becba3f68977c8cbd4fd
Ramasami <77200249+Ramasami@users.noreply.github.com>
2024-10-01 07:45:06 +0530

    Time: 7 ms (56.79%), Space: 56.5 MB (52.06%)

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -1,15 +1,25 @@
 class Solution {
     public boolean canArrange(int[] arr, int k) {
-        int[] m = new int[k];
-        for(int i: arr)
-            m[((i%k)+k)%k]++;
+        int[] m = new int[k/2+2];
+        for(int i: arr) {
+            if (i < 0)
+                i = (i%k)+k;
+            i%=k;
+            if (i!=0) {
+                if(i>k/2)
+                   m[k-i]--; 
+                else
+                    m[i]++;
+            }
+            
+        }
         for(int i=1;i<=(k-1)/2;i++) {
-            if(m[i]!=m[k-i]) {
+            if(m[i]!=0) {
                 return false;
             }
         }
-        if(k%2==0) {
-            if(m[k/2]%2==1) {
+        if((k&1)==0) {
+            if((m[k/2]&1)==1) {
                 return false;
                 
             }
